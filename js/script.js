3ed53a265c4669cf850e1527f5c97023356e4d4f
MattiaR <rovitomattia91@gmail.com>
2021-06-03 17:28:44 +0200

    add Fizz+Buzz+FizzBuzz+Boolean

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Step 1 Stampare numeri da 1 a 100
+
+// Step 2 Per i numeri multipli di 3 stampare la parola "Fizz" di fianco
+
+// Step 3 Per i numeri multipli di 5 stampare la parola "Buzz" di fianco
+
+// Step 4 Per i numeri multipli sia di 3 che di 5 stampare la parola "FizzBuzz" di fianco
+
+// Step 5 Bonus Se il numero è anche divisibile per 6, aggiungiamo "Boolean".6 diventerà quindi FizzBoolean, 30 diventerà FizzBuzzBoolean e via dicendo.
+
+
+// for (var i = 1; i <= 100; i++)  { 
+
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         document.getElementById('lista').innerHTML += i + ' FizzBuzz' + '<br>';
+//     } else if (i % 3 === 0){
+//         document.getElementById('lista').innerHTML += i + ' Fizz' + '<br>';
+//     } else if (i % 5 === 0){
+//         document.getElementById('lista').innerHTML += i + ' Buzz' + '<br>';
+//     }
+    
+// }
+
+
+
+for (var i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0) {
+        document.getElementById('lista').innerHTML += i + ' Fizz' + '<br>';
+    }
+
+    if (i % 5 === 0) {
+        document.getElementById('lista').innerHTML += i + ' Buzz' + '<br>';
+    }
+
+    if (i % 3 === 0 && i % 5 === 0){
+        document.getElementById('lista').innerHTML += i + ' FizzBuzz' + '<br>';
+    }
+
+    if (i % 6 === 0) {
+        document.getElementById('lista').innerHTML += i + ' Boolean' + '<br>';
+    }
+}
